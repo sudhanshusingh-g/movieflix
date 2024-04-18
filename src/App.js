f79d3b512c53e6518f6773e55ff57ceffa9eb815
sudhanshusingh-g <sudsingh32@gmail.com>
2024-04-18 15:52:05 +0530

    Routing added.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import './App.css';
-import Banner from './components/Banner/Banner';
 import Navbar from './components/Navbar/Navbar';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import WatchList from './pages/WatchList/WatchList';
+import HomePge from './pages/HomePage/HomePge';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Banner/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePge />} />
+          <Route path="/watchlist" element={<WatchList />} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/pages/HomePage/HomePge.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Banner from '../../components/Banner/Banner'
+function HomePge() {
+  return (
+    <div>
+        <Banner/>
+    </div>
+  )
+}
+
+export default HomePge
